5f52091b0ab959dbf70fba49027645ba977eee15
Selliott452 <selliott452@gmail.com>
2019-02-25 16:33:13 -0500

    Updated React App to Hello Wolrd

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,9 +7,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hello World
           </p>
           <a
             className="App-link"
@@ -17,7 +16,6 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
           </a>
         </header>
       </div>
